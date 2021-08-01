cfbd37085cd7f9cc11dd315da6e6ed705e83d89e
wenj <434679924@qq.com>
2021-08-01 23:33:05 +0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        List<Integer> list = new ArrayList<>();
+        list.add(6);
+
+        Class<? extends List> clazz = list.getClass();
+        Method add = clazz.getDeclaredMethod("add", Object.class);
+        add.invoke(list , "sss");
+
+        System.out.println(list);
+
+
+        Integer[] nums = {1,2,3};
+        String[] strs = {"A","B","C"};
+        printArray(nums);
+
+        printArray(strs);
+
+    }
+
+    public static <E> void printArray(E[] inputArray) {
+        for (E element : inputArray) {
+            System.out.printf("%s ", element);
+        }
+        System.out.println();
+    }
+
+}
